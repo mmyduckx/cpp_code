1465280ca2c9442462710ff9fb5a2b28b92f8265
mmyduckx <308303735@qq.com>
2020-06-21 19:58:21 -0400

    共享内存

---
+++ IPC/producer.cpp
@@ -0,0 +1,46 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/shm.h>
+#include "shm_com.h"
+
+int main(){
+    /*创建共享内存*/
+    int shmid;
+    shmid = shmget((key_t)1234, sizeof(struct shared_use_st), 0666 | IPC_CREAT);
+    if(shmid == -1){
+        fprintf(stderr, "shmget failed\n");
+        exit(EXIT_FAILURE);
+    }
+    /*将共享内存映射到进程的地址空间*/
+    void *shared_memory = (void *)0;
+    shared_memory = shmat(shmid, (void *)0 ,0);
+    if(shared_memory == (void *)-1){
+        fprintf(stderr, "shmat failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Memory attached at %X\n", (long)shared_memory);
+    struct shared_use_st *shared_stuff;
+    shared_stuff = (struct shared_use_st *)shared_memory;
+    int running = 1;
+    char buffer[BUFSIZ];
+    while(running){
+        while(shared_stuff->wrriten == 1){
+            sleep(1);
+            printf("Waiting for client...\n");
+        }
+        printf("Enter some text: ");
+        fgets(buffer, BUFSIZ, stdin);
+        strncpy(shared_stuff->text, buffer, TEXT_SZ);
+        shared_stuff->wrriten = 1;
+        if(strncmp(buffer, "end", 3) == 0) {
+            running = 0;
+        }
+    }
+    if(shmdt(shared_memory) == -1){
+        fprintf(stderr,"shmdt failed\n");
+        exit(EXIT_FAILURE);
+    }
+    exit(EXIT_SUCCESS);
+}
